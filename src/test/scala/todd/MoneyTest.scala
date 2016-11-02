615bf0a1d240393d694b087cd1f065b8be5566ac
Flanders <tflander@mifarloan-m1.local>
2016-11-02 19:36:42 -0400

    Free greens for non-round dime, big dime, and small dime

--- src/test/scala/todd/MoneyTest.scala
+++ src/test/scala/todd/MoneyTest.scala
@@ -37,19 +37,18 @@ class MoneyTest extends FunSpec with ShouldMatchers {
       Coin(ROUND, size=17.91, weight=2.27).valid should be(true)
     }
 
-// TODO: follow quarter pattern for testing dimes
-//    it("rejects a non-round dime") {
-//      Coin(NON_ROUND, size=24.26, weight=5.67).valid should be(false)      
-//    }
-//    
-//    it("rejects a big dime") {
-//      Coin(ROUND, size=24.27, weight=5.67).valid should be(false)      
-//    }
-//    
-//    it("rejects a small dime") {
-//      Coin(ROUND, size=24.25, weight=5.67).valid should be(false)      
-//    }
-//    
+    it("rejects a non-round dime") {
+      Coin(NON_ROUND, size=17.91, weight=2.27).valid should be(false)      
+    }
+    
+    it("rejects a big dime") {
+      Coin(ROUND, size=17.92, weight=2.27).valid should be(false)
+    }
+    
+    it("rejects a small dime") {
+      Coin(ROUND, size=17.90, weight=2.27).valid should be(false)
+    }
+    
 //    it("rejects a heavy dime") {
 //      Coin(ROUND, size=24.26, weight=5.68).valid should be(false)      
 //    }
