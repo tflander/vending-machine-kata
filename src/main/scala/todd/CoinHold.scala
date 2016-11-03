595ed38efe95fb6403d0a0a4e6e0c0dc57dfa296
Flanders <tflander@mifarloan-m1.corp.local>
2016-11-03 15:40:27 -0400

    first CoinHold test

--- src/main/scala/todd/CoinHold.scala
+++ src/main/scala/todd/CoinHold.scala
@@ -1,6 +1,17 @@
 package todd
 
 class CoinHold {
-  val coins = new scala.collection.mutable.ListBuffer[Coin]()
   
+  // TODO: remove coins and re-implement VendingMachine
+  val coins = new scala.collection.mutable.ListBuffer[Coin]()
+
+  var quarters = 0
+  
+  def insert(coin: Coin) = {
+    quarters += 1
+  }
+  
+  def insertedAmount: Int = {
+    quarters * 25
+  }
 }
--- src/test/scala/todd/CoinHoldTest.scala
+++ src/test/scala/todd/CoinHoldTest.scala
@@ -1,6 +1,21 @@
 package todd
 import org.scalatest._
 
-class CoinHoldTest {
+class CoinHoldTest extends FunSpec with ShouldMatchers {
+
+  val quarter = Coin(ROUND, size=24.26, weight=5.67)
+  val dime = Coin(ROUND, size=17.91, weight=2.27)
+  val nickel = Coin(ROUND, size=21.21, weight=5.00)
+  val penny = Coin(ROUND, size=19.05, weight=2.50)
   
+  describe("add coins") {
+    
+    it("accepts quarters") {
+      val coinHold = new CoinHold()
+      coinHold.insert(quarter)
+      coinHold.insertedAmount should be(25)
+      coinHold.quarters should be(1)
+    }
+    
+  }
 }
