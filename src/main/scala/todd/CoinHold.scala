fac27e2c2caec2dbbc54b574241bd0bf4d82a1f5
Flanders <tflander@mifarloan-m1.corp.local>
2016-11-03 16:16:00 -0400

    track separate coin counts in the coin holder

--- src/main/scala/todd/CoinHold.scala
+++ src/main/scala/todd/CoinHold.scala
@@ -5,13 +5,28 @@ class CoinHold {
   // TODO: remove coins and re-implement VendingMachine
   val coins = new scala.collection.mutable.ListBuffer[Coin]()
 
+  import Money._
   var quarters = 0
+  var dimes = 0
+  var nickels = 0
   
   def insert(coin: Coin) = {
-    quarters += 1
+    val coinWithValue = validate(coin)
+    coinWithValue match {
+      case Some(goodCoin) => {
+        goodCoin.value match {
+          case quarter.value => quarters += 1
+          case dime.value => dimes += 1
+          case nickel.value => nickels += 1
+        }
+      }
+      case None => {}
+    }
   }
   
   def insertedAmount: Int = {
-    quarters * 25
+    quarters * quarter.value +
+    dimes * dime.value +
+    nickels * nickel.value
   }
 }
--- src/test/scala/todd/CoinHoldTest.scala
+++ src/test/scala/todd/CoinHoldTest.scala
@@ -12,6 +12,26 @@ class CoinHoldTest extends FunSpec with ShouldMatchers {
       coinHold.insert(quarter)
       coinHold.insertedAmount should be(25)
       coinHold.quarters should be(1)
+      coinHold.nickels should be(0)
+      coinHold.dimes should be(0)
+    }
+    
+    it("accepts nickels") {
+      val coinHold = new CoinHold()
+      coinHold.insert(nickel)
+      coinHold.insertedAmount should be(5)
+      coinHold.quarters should be(0)
+      coinHold.nickels should be(1)
+      coinHold.dimes should be(0)
+    }
+    
+    it("accepts dimes") {
+      val coinHold = new CoinHold()
+      coinHold.insert(dime)
+      coinHold.insertedAmount should be(10)
+      coinHold.quarters should be(0)
+      coinHold.nickels should be(0)
+      coinHold.dimes should be(1)
     }
     
   }
