4d5a7e9c3624e7ee6d5d402868e360b4dc9df7b8
Flanders <tflander@mifarloan-m1.local>
2016-11-02 20:11:37 -0400

    clean code refactor

--- src/main/scala/todd/Money.scala
+++ src/main/scala/todd/Money.scala
@@ -1,39 +1,56 @@
 package todd
 
-/*
-Quarter
-24.26 mm
-5.67 g
-1965–present
-
-Nickel
-21.21 mm
-5 g
-1866–present
-
-Dime
-17.91 mm
-2.268 g
-1965–present	
-
-Small Cent
-19.05 mm
-2.5 g
-1857–present 
- */
-
 sealed trait CoinShape {}
 case object ROUND extends CoinShape
 case object NON_ROUND extends CoinShape
 
-case class Coin(shape: CoinShape, size: Double, weight: Double) {
-  val valid = if(shape == NON_ROUND) false else {
-    if(size == 24.26) {
-      weight == 5.67
-    } else if (size == 17.91) {
-      weight == 2.27
-    } else if (size == 21.21) {
-      weight == 5.00
-    } else false
+sealed trait ValidCoin {
+  def shape: CoinShape
+  def size: Double
+  def weight: Double
+  def value: Int
+  def name: String
+}
+
+case object Quarter extends ValidCoin {
+  val shape = ROUND
+  val size = 24.26
+  val weight = 5.67
+  val value = 25
+  val name = "Quarter"  
+}
+
+case object Dime extends ValidCoin {
+  val shape = ROUND
+  val size = 17.91
+  val weight = 2.27
+  val value = 10
+  val name = "Dime"  
+}
+
+case object Nickel extends ValidCoin {
+  val shape = ROUND
+  val size = 21.21
+  val weight = 5.00
+  val value = 5
+  val name = "Nickel"  
+}
+
+object Money {
+  val validCoins = Set(Quarter, Dime, Nickel)
+  
+  def validate(coin: Coin): Option[ValidCoin] = {
+    for (validCoin <- Money.validCoins) {
+      if(coin.shape == validCoin.shape && coin.size == validCoin.size && coin.weight == validCoin.weight) {
+        return Some(validCoin)
+      }
+    }
+    return None
   }
+  
+}
+
+case class Coin(shape: CoinShape, size: Double, weight: Double) {
+  val coin = Money.validate(this)
+  val valid = coin != None
 }
