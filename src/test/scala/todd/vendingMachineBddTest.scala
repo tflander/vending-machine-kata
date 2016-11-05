9faff8cbb179252a2340041917438047ba1e0c92
Flanders <tflander@mifarloan-m1.local>
2016-11-05 11:12:19 -0400

    create stubs for BDD Exact Change tests

--- src/test/scala/todd/vendingMachineBddTest.scala
+++ src/test/scala/todd/vendingMachineBddTest.scala
@@ -308,18 +308,33 @@ So that I can determine if I can buy something with the money I have before inse
 When the machine is not able to make change with the money in the machine for any of the items that it sells, it will 
 display EXACT CHANGE ONLY instead of INSERT COIN.
      */
+    // Assumption:  we need either four nickels, or one dime two nickels to make change.
+    // The advantage of this approach is that it decouples the money logic from the inventory logic.
+    // The disadvantage is that there are edge cases were we could get away with not requiring exact change.
     
-    // TODO: think about the cases where exact change is available for some items
-    // what is the minimal amount of change required to make change?
-    // assume you can return any coins not needed for purchase.  Probably should test for this.
+    it("requires exact change when the money box is empty") {
+      
+    }
     
-    //   coke is $1.00.  Could have 3 quarters and 3 dimes, requiring one nickel change
-    //   chips are $0.50.  Could have one quarter and 3 dimes, requiring one nickel change
-    //   candy is $0.65.  Could have three quarters, requiring one dime or two nickels change
-    //                    Could have two quarters and two dimes, requiring one nickel change
-    //   if something cost $0.05 and have a quarter, need two dimes, four nickels, or one dime two nickels.
+    it("requires exact change when the money box has three nickels") {
+      
+    }
     
-    // Assumption:  we need either two dimes, four nickels, or one dime two nickels to make change.
+    it("requires exact change when the money box has three nickels") {
+      
+    }
+    
+    it("requires exact change when the money box has one dime and one nickel") {
+      
+    }
+    
+    it("doesn't require exact change when the money box has one dime and two nickels") {
+      
+    }
+    
+    it("doesn't require exact change when the money box has four nickels") {
+      
+    }
   }
       
 
